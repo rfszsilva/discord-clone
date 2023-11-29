546802a25ce732f7e4da5f42c5c0904a5c61c0de
Rafael <rfsilva.corp@gmail.com>
2023-11-29 17:05:17 -0300

    Finalizada a criação de server API

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,127 +1,142 @@
-"use client"
+"use client";
 
-import {useForm} from "react-hook-form";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle    
-} from "@/components/ui/dialog"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
-} from "@/components/ui/form"
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { FileUpload } from "@/components/file-upload";
+import { useRouter } from "next/navigation";
+
+
 
 const formSchema = z.object({
-    name: z.string() .min(1, {
-        message: "Server name is required."
-    }),
-    imageUrl: z.string() .min(1, {
-        message: "Server image is required."
-    })
-})
+  name: z.string().min(1, {
+    message: "Server name is required.",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required.",
+  }),
+});
 
 export const InitialModal = () => {
-    const [ isMounted, setIsMounted] = useState (false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+  const router = useRouter();
 
-    const form = useForm ({
-        resolver: zodResolver(formSchema),
-        defaultValues: {    
-            name: "",
-            imageUrl: "",
-        }
-    });
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-const isLoading = form.formState.isSubmitting;
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
 
-const onSubmit =async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-}
+  const isLoading = form.formState.isSubmitting;
 
-if (!isMounted) {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+    await axios.post("/api/servers", values);
+
+    form.reset();
+    router.refresh();
+    window.location.reload();
+
+    } catch (error) {
+      console.log(error);  
+    }
+  };        
+
+  if (!isMounted) {
     return null;
-}
+  }
 
-    return  (
+  return (
     <Dialog open>
-        <DialogContent className="bg-white text-black p-0 overflow-hidden ">
-            <DialogHeader className="pt-8 px-6">
-                <DialogTitle className="text-2xl text-center">
-                    Customize seu servidor
-                </DialogTitle>
-                <DialogDescription className="text-center text-zinc-500">
-                    Dê ao seu servidor uma personalidade com nome e imagem. Você pode alterar quando quiser. 
-                </DialogDescription>
-            </DialogHeader>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-8">
-                    <div className=" space-y-8 px-6">
-                        <div className=" flex items-center justify-center text-center">
-                            <FormField
-                                control={form.control}
-                                name="imageUrl"
-                                render= {({ field }) => (
-                                    <FormItem>
-                                        <FormControl>
-                                            <FileUpload
-                                            value={field.value}
-                                            endpoint="serverImage"
-                                            onChange={field.onChange}
-                                            />                                            
-                                        </FormControl>
-                                    </FormItem>
-                                    )}
+      <DialogContent className="bg-white text-black p-0 overflow-hidden ">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center">
+            Customize seu servidor
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Dê ao seu servidor uma personalidade com nome e imagem. Você pode
+            alterar quando quiser.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-8">
+            <div className=" space-y-8 px-6">
+              <div className=" flex items-center justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          value={field.value}
+                          endpoint="serverImage"
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-                            />
-                        </div>
-
-                        <FormField
-                            control={form.control}
-                            name="name"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondry/70">
-                                        Server Name
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            disabled={isLoading}
-                                            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                                            placeholder="Enter server name"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                        />            
-                    </div>
-                    <DialogFooter className="bg-gray-100 py-6 px-4">
-                        <Button variant="primary" disabled={isLoading}>
-                            Create                        
-                        </Button>
-                    </DialogFooter>
-                </form>                
-            </Form>
-        </DialogContent>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondry/70">
+                      Server Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 py-6 px-4">
+              <Button variant="primary" disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
     </Dialog>
-    )
-}
+  );
+};
